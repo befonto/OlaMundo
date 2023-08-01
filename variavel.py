8ea0503f6132b3efb95d940757c489990f009b9b
Beatriz Fontoura <befonto@gamil>
2023-08-01 08:19:43 -0300

    Primeiro commit

---
+++ variavel.py
@@ -0,0 +1,12 @@
+# input é uma função que permite perguntar algo ao usuário
+nome = input("Qual é o seu nome?")
+
+'''
+Bloco
+de
+Comentário
+
+'''
+
+print("Olá,", nome, ". Tudo bem com você?")
+print("Alteração depois de clonado")
